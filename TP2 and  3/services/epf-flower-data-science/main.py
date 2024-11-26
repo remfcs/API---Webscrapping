827eb714af170bb2fdc3149217d43af746b2a337
Francis <remy.francis@epfedu.fr>
2024-11-26 10:57:51 +0100

    major feature /docs

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -6,3 +6,4 @@ app = get_application()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
+
--- TP2 and  3/services/epf-flower-data-science/src/api/router.py
+++ TP2 and  3/services/epf-flower-data-science/src/api/router.py
@@ -1,8 +1,12 @@
 """API Router for Fast API."""
 from fastapi import APIRouter
-
+from fastapi.responses import RedirectResponse
 from src.api.routes import hello
 
 router = APIRouter()
 
 router.include_router(hello.router, tags=["Hello"])
+
+@router.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs")
